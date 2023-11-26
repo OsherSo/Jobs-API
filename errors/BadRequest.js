b108a48dd5641af0a212a5884fb7d2cb37fccd05
Osher Solimany <solimani.osher@gmail.com>
2023-11-26 19:58:30 +0200

    Fix requires

--- errors/BadRequest.js
+++ errors/BadRequest.js
@@ -1,11 +1,12 @@
 const { StatusCodes } = require('http-status-codes');
-const CustomAPIError = require('./custom-api');
 
-class BadRequestError extends CustomAPIError {
+const CustomAPI = require('../errors/CustomAPI');
+
+class BadRequest extends CustomAPI {
   constructor(message) {
     super(message);
     this.statusCode = StatusCodes.BAD_REQUEST;
   }
 }
 
-module.exports = BadRequestError;
+module.exports = BadRequest;
