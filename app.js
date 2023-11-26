ca87f34db74b9a30619940d0ce0bcf99f85068b0
Osher Solimany <solimani.osher@gmail.com>
2023-11-26 19:59:35 +0200

    Remove useless lines

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
+
+const { notFound, errorHandler } = require('./middleware');
+
 const app = express();
 
-// error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
-
 app.use(express.json());
-// extra packages
 
-// routes
 app.get('/', (req, res) => {
   res.send('jobs api');
 });
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
 
