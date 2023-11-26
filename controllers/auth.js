253511ca3deb5469dcebefb264ae184d3fbab493
Osher Solimany <solimani.osher@gmail.com>
2023-11-26 22:15:23 +0200

    Add createJWT instance method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,3 @@
-const bcrypt = require('bcryptjs');
 const { StatusCodes } = require('http-status-codes');
 
 const User = require('../models/User');
@@ -10,8 +9,9 @@ const register = async (req, res) => {
     throw new BadRequest('Please provide all values');
 
   const user = await User.create({ name, email, password });
+  const token = user.createJWT();
 
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ user: { name }, token });
 };
 
 const login = async (req, res) => {
