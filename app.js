5c163f3918223b69f9db3126b0760823749163ea
Osher Solimany <solimani.osher@gmail.com>
2023-11-27 23:48:48 +0200

    Add security packages

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 
+const cors = require('cors');
+const xss = require('xss-clean');
+const helmet = require('helmet');
+const rateLimiter = require('express-rate-limit');
+
 const connectDB = require('./db/connect');
 const auth = require('./middleware/auth');
 const jobsRouter = require('./routes/jobs');
@@ -10,7 +15,17 @@ const { notFound, errorHandler } = require('./middleware');
 
 const app = express();
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', auth, jobsRouter);
