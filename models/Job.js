3c232695d7dbd4b182fdf0a925da8ff9058cf9ed
Osher Solimany <solimani.osher@gmail.com>
2023-11-27 20:39:21 +0200

    Add validator messages

--- models/Job.js
+++ models/Job.js
@@ -1,33 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const jobSchema = new mongoose.Schema(
   {
     company: {
       type: String,
       trim: true,
-      required: [true, ''],
-      minLength: [3, ''],
-      maxLength: [50, ''],
+      required: [true, "Company name is required"],
+      minLength: [3, "Company name must be at least 3 characters"],
+      maxLength: [50, "Company name cannot exceed 50 characters"],
     },
     position: {
       type: String,
       trim: true,
-      required: [true, ''],
-      minLength: [3, ''],
-      maxLength: [50, ''],
+      required: [true, "Position title is required"],
+      minLength: [3, "Position title must be at least 3 characters"],
+      maxLength: [50, "Position title cannot exceed 50 characters"],
     },
     status: {
       type: String,
-      enum: ['interview', 'declined', 'pending'],
-      default: 'pending',
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
     },
     createdBy: {
       type: mongoose.Types.ObjectId,
-      ref: 'User',
-      required: [true, ''],
+      ref: "User",
+      required: [true, "User ID is required for job creation"],
     },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model('Job', jobSchema);
+module.exports = mongoose.model("Job", jobSchema);
