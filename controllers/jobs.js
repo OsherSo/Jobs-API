0b8c48d781629494d08b10b0ffd8a5e5ef53ae88
Osher Solimany <solimani.osher@gmail.com>
2023-11-29 21:18:47 +0200

    Add filter by status and jobType, sort and pagination

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -4,16 +4,38 @@ const Job = require('../models/Job');
 const { NotFound } = require('../errors');
 
 const getAllJobs = async (req, res) => {
-  const { search } = req.query;
+  const { search, status, jobType, sort } = req.query;
   const queryObj = { createdBy: req.user.userId };
 
   if (search) queryObj.position = { $regex: search, $options: 'i' };
+  if (status && status !== 'all') queryObj.status = status;
+  if (jobType && jobType !== 'all') queryObj.jobType = jobType;
 
-  const jobs = await Job.find(queryObj);
+  let jobsQuery = Job.find(queryObj);
+
+  if (sort) {
+    const lut = {
+      latest: () => (jobsQuery = jobsQuery.sort('-createdAt')),
+      oldest: () => (jobsQuery = jobsQuery.sort('createdAt')),
+      'a-z': () => (jobsQuery = jobsQuery.sort('position')),
+      'z-a': () => (jobsQuery = jobsQuery.sort('-position')),
+    };
+    if (lut[sort]) lut[sort]();
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  jobsQuery = jobsQuery.skip(skip).limit(limit);
+
+  const jobs = await jobsQuery;
+  const totalJobs = await Job.countDocuments(queryObj);
+  const numOfPages = Math.ceil(totalJobs / limit);
 
   res.status(StatusCodes.OK).json({
-    count: jobs.length,
     jobs,
+    totalJobs,
+    numOfPages,
   });
 };
 
