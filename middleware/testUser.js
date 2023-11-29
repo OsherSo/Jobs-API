2578b1d50610540ebea6291b6ee4798585fdc1fc
Osher Solimany <solimani.osher@gmail.com>
2023-11-30 00:50:05 +0200

    Remove console.log

--- middleware/testUser.js
+++ middleware/testUser.js
@@ -1,7 +1,6 @@
 const { BadRequest } = require('../errors');
 
 const testUser = (req, res, next) => {
-  console.log(req.user);
   if (req.user.testUser) throw new BadRequest('Test User. Read Only!');
   next();
 };
